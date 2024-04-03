33b9987056eb7ac8c568c3a37e582c5d665cea66
Sachintha <sudesh@orelit.com>
2024-04-03 17:46:57 +0530

    add the logger

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import app from './app'
 import dotenv from 'dotenv'
+import Logger from './src/utils/logger'
 
 dotenv.config()
 
 const PORT = process.env.port || 3000
-app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`)
-})
+app
+  .listen(PORT, () => {
+    Logger.info(`
+      ################################################
+       🛡️   Server listening on port: ${PORT}      🛡️
+      ################################################
+    `)
+  })
+  .on('error', err => {
+    Logger.error(err)
+    process.exit(1)
+  })
