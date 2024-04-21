48fa4f60bf6101eab5f21affeaeb3657bf11f883
Sachintha <sudesh@orelit.com>
2024-04-21 14:40:31 +0530

    ft:db connection and redis client connection add

--- config/default.ts
+++ config/default.ts
@@ -12,10 +12,11 @@ export default {
   refreshTokenExpiresIn: 2592000, // 30 days in seconds
   accessTokenExpiresIn: 2592000, // 30 days in seconds
   // origin:
-  //redisurl:
-  //redishost:
-  //redisport:
-  //redispassword: ,
+  redisurl:
+    'redis://default:HyOciXNdF3en5lPuudx2dw5OSR4b7dVRGAzCaEceKFQ=@redis-authserver.redis.cache.windows.net:17352',
+  redishost: 'authserver.redis.cache.windows.net',
+  redisport: 6380,
+  redispassword: 'HyOciXNdF3en5lPuudx2dw5OSR4b7dVRGAzCaEceKFQ=',
   accessTokenPrivateKey: process.env.JWT_ACCESS_TOKEN_PRIVATE_KEY,
   accessTokenPublicKey: process.env.JWT_ACCESS_TOKEN_PUBLIC_KEY,
   refreshTokenPrivateKey: process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY,
