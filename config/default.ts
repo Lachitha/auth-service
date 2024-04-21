3d50322fe0eb63c9c642fe6483c7d0593929e50c
Sachintha <sudesh@orelit.com>
2024-04-21 19:05:05 +0530

    fix:security issue

--- config/default.ts
+++ config/default.ts
@@ -12,11 +12,10 @@ export default {
   refreshTokenExpiresIn: 2592000, // 30 days in seconds
   accessTokenExpiresIn: 2592000, // 30 days in seconds
   // origin:
-  redisurl:
-    'redis://default:HyOciXNdF3en5lPuudx2dw5OSR4b7dVRGAzCaEceKFQ=@redis-authserver.redis.cache.windows.net:17352',
-  redishost: 'authserver.redis.cache.windows.net',
-  redisport: 6380,
-  redispassword: 'HyOciXNdF3en5lPuudx2dw5OSR4b7dVRGAzCaEceKFQ=',
+  redisurl: process.env.REDIS_URL,
+  redishost: process.env.REDIS_HOST,
+  redisport: process.env.REDIS_PORT,
+  redispassword: process.env.REDIS_PASSWORD,
   accessTokenPrivateKey: process.env.JWT_ACCESS_TOKEN_PRIVATE_KEY,
   accessTokenPublicKey: process.env.JWT_ACCESS_TOKEN_PUBLIC_KEY,
   refreshTokenPrivateKey: process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY,
